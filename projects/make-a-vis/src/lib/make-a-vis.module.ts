de5cab4c6c373c576b91cf05152052ae3f0312f4
axdanbol <daniel_bolin@yahoo.com>
2018-08-14 11:16:15 -0400

    Changed naming + imported in main module

--- projects/make-a-vis/src/lib/make-a-vis.module.ts
+++ projects/make-a-vis/src/lib/make-a-vis.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
+import { DataViewModule } from './data-view/data-view.module';
+import { LegendViewModule } from './legend-view/legend-view.module';
+import { ToolbarModule } from './toolbar/toolbar.module';
+import { VisualizationViewModule } from './visualization-view/visualization-view.module';
+
 import { MakeAVisComponent } from './make-a-vis.component';
 
 @NgModule({
-  imports: [
-  ],
+  imports: [DataViewModule, LegendViewModule, ToolbarModule, VisualizationViewModule],
   declarations: [MakeAVisComponent],
   exports: [MakeAVisComponent]
 })
