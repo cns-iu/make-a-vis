4da20dd524e7785880355659aed08c6fceb5972c
axdanbol <daniel_bolin@yahoo.com>
2018-08-15 15:10:20 -0400

    Tests for default logger

--- projects/make-a-vis/src/lib/logging/default/logger-factory.service.spec.ts
+++ projects/make-a-vis/src/lib/logging/default/logger-factory.service.spec.ts
@@ -1,15 +1,31 @@
 import { TestBed, inject } from '@angular/core/testing';
-
+import { Logger } from '../logger';
+import { LoggerFactory } from '../logger-factory';
+import { DefaultLogger } from './logger';
 import { DefaultLoggerFactoryService } from './logger-factory.service';
 
+describe('Logging', () => {
 describe('DefaultLoggerFactoryService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [DefaultLoggerFactoryService]
+      providers: [LoggerFactory.use(DefaultLoggerFactoryService), Logger.for()]
     });
   });
 
-  it('should be created', inject([DefaultLoggerFactoryService], (service: DefaultLoggerFactoryService) => {
+  it('creates', inject([LoggerFactory], (service: LoggerFactory) => {
     expect(service).toBeTruthy();
   }));
+
+  it('is of type DefaultLoggerFactoryService', inject([LoggerFactory], (service: LoggerFactory) => {
+    expect(service).toEqual(jasmine.any(DefaultLoggerFactoryService));
+  }));
+
+  it('creates loggers', inject([Logger], (logger: Logger) => {
+    expect(logger).toBeTruthy();
+  }));
+
+  it('creates loggers of type DefaultLogger', inject([Logger], (logger: Logger) => {
+    expect(logger).toEqual(jasmine.any(DefaultLogger));
+  }));
+});
 });
