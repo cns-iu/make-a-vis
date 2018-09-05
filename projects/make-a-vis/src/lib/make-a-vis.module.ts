89d778c8303805f16edd9652e38ed254249b3ffa
axdanbol <daniel_bolin@yahoo.com>
2018-09-05 15:49:44 -0400

    Removed more deprecated logging code

--- projects/make-a-vis/src/lib/make-a-vis.module.ts
+++ projects/make-a-vis/src/lib/make-a-vis.module.ts
@@ -1,11 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-// Logging
-import { Logger } from './logging/logger';
-import { LoggerFactory } from './logging/logger-factory';
-import { NullLoggerFactory } from './logging/null-logging/logger-factory';
-
 // Submodules
 import { DataViewModule } from './data-view/data-view.module';
 import { LegendViewModule } from './legend-view/legend-view.module';
@@ -25,14 +20,6 @@ import { MakeAVisComponent } from './make-a-vis.component';
   ],
   declarations: [LightThemeComponent, MakeAVisComponent],
   exports: [MakeAVisComponent, ToolbarModule],
-  providers: [
-    LoggerFactory.use(NullLoggerFactory),
-    // Copied from Logger.for because Angular's aot compiler is dumber than a log.
-    {
-      provide: Logger,
-      useFactory(factory: LoggerFactory): Logger { return factory.createLogger('mav'); },
-      deps: [LoggerFactory]
-    }
-  ]
+  providers: []
 })
 export class MakeAVisModule { }
