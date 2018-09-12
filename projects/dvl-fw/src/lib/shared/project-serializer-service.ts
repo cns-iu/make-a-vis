7053dd203a66169b3a63610fc5b05c70d7ec6c07
Bruce Herr II <bherr@indiana.edu>
2018-09-12 11:59:45 -0400

    Added a createProject function to the project serializer service, updated exports, and some other minor fixes

--- projects/dvl-fw/src/lib/shared/project-serializer-service.ts
+++ projects/dvl-fw/src/lib/shared/project-serializer-service.ts
@@ -1,3 +1,4 @@
+import { ISITemplateProject } from './../plugins/isi/isi-template-project';
 import { ProjectSerializer } from './project-serializer';
 import { Injectable } from '@angular/core';
 import { ObjectFactoryRegistry } from './object-factory';
@@ -12,6 +13,17 @@ export class ProjectSerializerService {
 
   constructor(/* TODO: Inject Plugins */) {
     this.registry = ProjectSerializer.defaultRegistry;
+  }
+
+  createProject(template: 'isi' | 'nsf' | 'csv' | 'json', fileContents: string): Observable<Project> {
+    return defer<Project>(async () => {
+      switch (template) {
+        case 'isi':
+          return await ISITemplateProject.create(fileContents);
+        default:
+          throw new Error(`Template: ${template} not supported.`);
+      }
+    });
   }
 
   toYAML(project: Project): Observable<string> {
--- projects/dvl-fw/src/public_api.ts
+++ projects/dvl-fw/src/public_api.ts
@@ -3,6 +3,26 @@
  */
 
 export * from './lib/dvl-fw.module';
+
+// API
+export * from './lib/shared/data-source';
+export * from './lib/shared/data-variable';
+export * from './lib/shared/graphic-symbol';
+export * from './lib/shared/graphic-variable';
 export * from './lib/shared/project-serializer-service';
 export * from './lib/shared/project';
-export * from './lib/plugins/isi/isi-template-project';
+export * from './lib/shared/raw-data';
+export * from './lib/shared/record-set';
+export * from './lib/shared/record-stream';
+export * from './lib/shared/visualization';
+
+// Default Implementations
+export * from './lib/plugins/default/default-data-source';
+export * from './lib/plugins/default/default-data-variable';
+export * from './lib/plugins/default/default-graphic-symbol';
+export * from './lib/plugins/default/default-graphic-variable';
+export * from './lib/plugins/default/default-project';
+export * from './lib/plugins/default/default-raw-data';
+export * from './lib/plugins/default/default-record-set';
+export * from './lib/plugins/default/default-record-stream';
+export * from './lib/plugins/default/default-visualization';
