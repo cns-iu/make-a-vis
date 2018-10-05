42d0a9a074db99e7a802cbcd999ec21d66dd43f5
axdanbol <daniel_bolin@yahoo.com>
2018-10-05 11:30:33 -0400

    Improved visualization view

--- projects/make-a-vis/src/lib/visualization-view/shared/store/actions.ts
+++ projects/make-a-vis/src/lib/visualization-view/shared/store/actions.ts
@@ -1,42 +1,46 @@
 import { Action } from '@ngrx/store';
+import { Visualization } from 'dvl-fw';
 
 // import { DataSource, Visualization, RecordStream } from 'dvl-fw';
 
 export enum VisualizationActionTypes {
   SetActiveVisualization = '[VIS] Set Active Visualization',
   AddNewVisualization = '[VIS] Add New Visualization',
-  SetGraphicSymbolRecordSet = '[VIS] Set Graphic Symbol Record Set',
+  RemoveVisualization = '[VIS] Remove Visualization',
 
+  SetGraphicSymbolRecordSet = '[VIS] Set Graphic Symbol Record Set',
   SetActiveDataVariable = '[VIS] Set Active Data Variable',
   SetGraphicVariable = '[VIS] Set Graphic Variable',
 }
 
 export class SetActiveVisualization implements Action {
   readonly type = VisualizationActionTypes.SetActiveVisualization;
-  constructor(public payload = {}) {
-  }
+  constructor(public index: number) { }
 }
 
 export class AddNewVisualization implements Action {
   readonly type = VisualizationActionTypes.AddNewVisualization;
-  constructor(public payload = {}) {
-  }
+  constructor(public visualization: Visualization) { }
+}
+
+export class RemoveVisualization implements Action {
+  readonly type = VisualizationActionTypes.RemoveVisualization;
+  constructor(public index: number) { }
 }
 
 export class SetGraphicSymbolRecordSet implements Action {
   readonly type = VisualizationActionTypes.SetGraphicSymbolRecordSet;
-  constructor(public payload = {}) {
-  }
+  constructor(public payload = {}) { }
 }
 
 export class SetActiveDataVariable implements Action {
   readonly type = VisualizationActionTypes.SetActiveDataVariable;
-  constructor(public payload = {}) {
-  }
+  constructor(public payload = {}) { }
 }
 
 export class SetGraphicVariable implements Action {
   readonly type = VisualizationActionTypes.SetGraphicVariable;
-  constructor(public payload = {}) {
-  }
+  constructor(public payload = {}) { }
 }
+
+export type VisualizationAction = SetActiveVisualization | AddNewVisualization | RemoveVisualization;
--- projects/make-a-vis/src/lib/visualization-view/shared/store/reducer.ts
+++ projects/make-a-vis/src/lib/visualization-view/shared/store/reducer.ts
@@ -1,33 +1,37 @@
 import { VisualizationState, INITIAL_VISUALIZATION_STATE } from './state';
 
-import { VisualizationActionTypes } from './actions';
+import { VisualizationAction, VisualizationActionTypes } from './actions';
 
 export function visualizationStateReducer (
   state: VisualizationState = INITIAL_VISUALIZATION_STATE,
-  action: any
-) {
-
+  action: VisualizationAction
+): VisualizationState {
     const newState: VisualizationState = Object.assign({}, state);
 
-    switch (action) {
+    switch (action.type) {
       case VisualizationActionTypes.SetActiveVisualization:
-        newState.activeVisualization = action.payload;
+        newState.activeVisualization = action.index;
         return newState;
 
       case VisualizationActionTypes.AddNewVisualization:
-        newState.activeVisualization = action.payload;
+        newState.visualizations = state.visualizations.concat(action.visualization);
         return newState;
 
-      case VisualizationActionTypes.SetGraphicSymbolRecordSet:
+      case VisualizationActionTypes.RemoveVisualization:
+        newState.visualizations = state.visualizations.slice();
+        newState.visualizations.splice(action.index, 1);
         return newState;
 
-      case VisualizationActionTypes.SetActiveDataVariable:
-        newState.activeDataVariable = action.payload;
-        return newState;
+      // case VisualizationActionTypes.SetGraphicSymbolRecordSet:
+      //   return newState;
 
-      case VisualizationActionTypes.SetGraphicVariable:
-        //
-        return newState;
+      // case VisualizationActionTypes.SetActiveDataVariable:
+      //   newState.activeDataVariable = action.payload;
+      //   return newState;
+
+      // case VisualizationActionTypes.SetGraphicVariable:
+      //   //
+      //   return newState;
 
       default:
         return state;
