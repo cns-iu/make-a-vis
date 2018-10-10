1137b7421a38546c06285ac31b132217151be003
axdanbol <daniel_bolin@yahoo.com>
2018-10-10 11:07:50 -0400

    Moved some dino plugin files around

--- projects/dvl-fw/src/lib/plugins/ngx-dino/components/network/network-visualization.ts
+++ projects/dvl-fw/src/lib/plugins/ngx-dino/components/network/network-visualization.ts
@@ -1,8 +1,8 @@
-import { Visualization } from '../../shared/visualization';
-import { Project } from '../../shared/project';
-import { ObjectFactory, ObjectFactoryRegistry } from '../../shared/object-factory';
-import { DefaultVisualization } from '../default/default-visualization';
-import { NetworkComponent } from './components/network/network.component';
+import { Visualization } from '../../../../shared/visualization';
+import { Project } from '../../../../shared/project';
+import { ObjectFactory, ObjectFactoryRegistry } from '../../../../shared/object-factory';
+import { DefaultVisualization } from '../../../default/default-visualization';
+import { NetworkComponent } from './network.component';
 
 export class NetworkVisualization extends DefaultVisualization {
   readonly component = NetworkComponent;
--- projects/dvl-fw/src/lib/plugins/ngx-dino/ngx-dino-plugin.ts
+++ projects/dvl-fw/src/lib/plugins/ngx-dino/ngx-dino-plugin.ts
@@ -1,8 +1,8 @@
 import { ObjectFactoryPlugin, ObjectFactoryRegistry } from '../../shared/object-factory';
-import { ScatterplotVisualizationFactory } from './scatterplot-visualization';
-import { GeomapVisualizationFactory } from './geomap-visualization';
-import { NetworkVisualizationFactory } from './network-visualization';
-import { SciencemapVisualizationFactory } from './sciencemap-visualization';
+import { ScatterplotVisualizationFactory } from './components/scatterplot/scatterplot-visualization';
+import { GeomapVisualizationFactory } from './components/geomap/geomap-visualization';
+import { NetworkVisualizationFactory } from './components/network/network-visualization';
+import { SciencemapVisualizationFactory } from './components/sciencemap/sciencemap-visualization';
 
 
 export class NgxDinoPlugin implements ObjectFactoryPlugin {
