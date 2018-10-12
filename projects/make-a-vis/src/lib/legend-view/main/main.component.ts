7e0df2cdc195d6cca3959127265ac84f5d54daf9
axdanbol <daniel_bolin@yahoo.com>
2018-10-12 15:12:31 -0400

    Listen to visualization change in legend

--- projects/make-a-vis/src/lib/legend-view/main/main.component.ts
+++ projects/make-a-vis/src/lib/legend-view/main/main.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
+import { concatMap, distinctUntilChanged, pluck } from 'rxjs/operators';
+import { Store, select } from '@ngrx/store';
+import { GraphicSymbolOption, Project } from 'dvl-fw';
+import { ApplicationState, getUiFeature } from '../../shared/store';
 
 @Component({
   selector: 'mav-legend-view',
@@ -6,8 +11,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  options: GraphicSymbolOption[] = [];
   legendTypes = ['Nodes', 'Edges', 'States'];
-  constructor() { }
+
+  constructor(public store: Store<ApplicationState>) {
+    const emptyArray = [];
+
+    store.pipe(
+      select(getUiFeature),
+      concatMap(({ activeVisualization: index, project}) => {
+        const hasVis = project && index >= 0;
+        return hasVis ? of(project).pipe(
+          pluck<Project, GraphicSymbolOption[]>('visualizations', String(index), 'graphicSymbolOptions')
+        ) : of<GraphicSymbolOption[]>(emptyArray);
+      }),
+      distinctUntilChanged()
+    ).subscribe(options => {
+      this.options = options;
+    });
+  }
 
   ngOnInit() {
   }
