078532854cae1e984e313a238a1c4ddf359a96ee
Bruce Herr II <bherr@indiana.edu>
2018-10-22 09:18:17 -0400

    Found the code that was failing when loading a --prod built client. Logger.setLevel is getting called too early and something is null. @axdanbol please see what to do about this one.

--- projects/make-a-vis/src/lib/make-a-vis.module.ts
+++ projects/make-a-vis/src/lib/make-a-vis.module.ts
@@ -46,6 +46,7 @@ import { LogActions } from './shared/logging/log';
 })
 export class MakeAVisModule {
   constructor(loggingControl: LoggingControlService, logger: Logger) {
-    logger.setLevel(LogLevel.Trace);
+    // TODO: Figure out where to put this. Its getting called too early when building with --prod.
+    // logger.setLevel(LogLevel.Trace);
   }
 }
