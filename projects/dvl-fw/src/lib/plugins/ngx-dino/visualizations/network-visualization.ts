5442ccbf3349a17f567c6b17630a0ea8fa72edcc
Gagandeep Singh <singhgag@indiana.edu>
2018-10-22 11:53:14 -0400

    corrected stroke color title label + added label in isi template for author record stream

--- projects/dvl-fw/src/lib/plugins/ngx-dino/visualizations/network-visualization.ts
+++ projects/dvl-fw/src/lib/plugins/ngx-dino/visualizations/network-visualization.ts
@@ -15,7 +15,7 @@ export class NetworkVisualization extends DefaultVisualization {
         { type: 'areaSize', label: 'Area Size', visualization: 'node-size'},
         { type: 'shape', label: 'Shape' },
         { type: 'color', label: 'Color', visualization: 'color' },
-        { type: 'strokeColor', label: 'strokeColor', visualization: 'color' }
+        { type: 'strokeColor', label: 'Stroke Color', visualization: 'color' }
       ]
     }, {
       id: 'edges', label: 'Edges', type: 'line',
