bc33a49f4306b129b8c37308d62c7c94587f3e0e
Bruce Herr II <bherr@indiana.edu>
2018-11-01 10:56:11 -0400

    Add some more exports to dvl-fw exports

--- projects/dvl-fw/src/public_api.ts
+++ projects/dvl-fw/src/public_api.ts
@@ -9,11 +9,14 @@ export * from './lib/shared/data-source';
 export * from './lib/shared/data-variable';
 export * from './lib/shared/graphic-symbol';
 export * from './lib/shared/graphic-variable';
+export * from './lib/shared/object-factory';
 export * from './lib/shared/project-serializer-service';
+export * from './lib/shared/project-serializer';
 export * from './lib/shared/project';
 export * from './lib/shared/raw-data';
 export * from './lib/shared/record-set';
 export * from './lib/shared/record-stream';
+export * from './lib/shared/transient';
 export * from './lib/shared/visualization-component';
 export * from './lib/shared/visualization';
 
@@ -22,12 +25,19 @@ export * from './lib/plugins/default/default-data-source';
 export * from './lib/plugins/default/default-data-variable';
 export * from './lib/plugins/default/default-graphic-symbol';
 export * from './lib/plugins/default/default-graphic-variable';
+export * from './lib/plugins/default/default-plugin';
 export * from './lib/plugins/default/default-project';
 export * from './lib/plugins/default/default-raw-data';
 export * from './lib/plugins/default/default-record-set';
 export * from './lib/plugins/default/default-record-stream';
 export * from './lib/plugins/default/default-visualization';
+
+// Other Plugins/Implementations
+export * from './lib/plugins/activity-log/log-plugin';
 export * from './lib/plugins/activity-log/log-raw-data';
+export * from './lib/plugins/isi/isi-plugin';
+export * from './lib/plugins/nsf/nsf-plugin';
+export * from './lib/plugins/ngx-dino/ngx-dino-plugin';
 
 // Components
 export * from './lib/visualization/visualization.component';
