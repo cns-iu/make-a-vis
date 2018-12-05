68a474167134e722f67d50ebb058f5d20a600000
Shashank Shekhar <shashekh@iu.edu>
2018-12-05 13:22:58 -0500

    added footer, and improved build info code, buttons are taken from mockup

--- projects/make-a-vis/src/lib/toolbar/info-dialog/build-info.ts
+++ projects/make-a-vis/src/lib/toolbar/info-dialog/build-info.ts
@@ -1,8 +1,5 @@
-
 export const buildInfo = {
   'version': '0.5.0',
-  'lastCommitDate': '2018-12-4 14:26:26',
-  'buildDate': '2018-12-05T16:46:36.085Z'
+  'lastCommitDate': '2018-12-5 11:58:42',
+  'buildDate': '2018-12-05T17:22:10.175Z'
 };
-
-
--- projects/make-a-vis/src/lib/toolbar/info-dialog/info-dialog.component.ts
+++ projects/make-a-vis/src/lib/toolbar/info-dialog/info-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import {  buildInfo } from './build-info';
+import { stringCompare } from '@ngx-dino/core/lib/common';
 @Component({
   selector: 'mav-info-dialog',
   templateUrl: './info-dialog.component.html',
@@ -11,9 +12,13 @@ export class InfoDialogComponent implements OnInit {
   infoItems: Array<InfoItem>;
   footer: Array<any>;
   headerText: string;
+  aboutusContent: any;
+  contactusContent: any;
 
   constructor(public dialogRef: MatDialogRef<InfoDialogComponent>) {
       this.createItems();
+      this.createAboutusContent();
+      this.createContactusContent();
       this.headerText = 'IVC Make-A-Vis';
       this.footer = [1, 2, 3]; // TODO: do something to create footer items
     }
@@ -33,19 +38,25 @@ export class InfoDialogComponent implements OnInit {
       title: 'How to make a network graph',
       description: '',
       content: ''
-    }, {
+    }];
+  }
+
+  createAboutusContent() {
+    this.aboutusContent = {
       title: 'About Make-A-Vis',
       description: '',
-      content: {
-        'version': buildInfo['version'],
-        'lastCommitDate': new Date(buildInfo['lastCommitDate']),
-        'buildDate': new Date('buildDate')
-      }
-    }, {
-      title: 'Contanct Us',
+      content: [{'label': 'Version', 'value': buildInfo['version']},
+      {'label': 'Last commit Date', 'value': new Date(buildInfo['lastCommitDate']).toLocaleDateString('en-US')},
+      {'label': 'Build Date', 'value': new Date(buildInfo['buildDate']).toLocaleDateString('en-US')}]
+    };
+  }
+
+  createContactusContent() {
+    this.contactusContent =  {
+      title: 'Contact Us',
       description: '',
-      content: ''
-    }];
+      content: []
+    };
   }
 }
 
