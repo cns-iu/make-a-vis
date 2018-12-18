2d600a8b23c084fb7d0fef39e67ff2ac8770821f
Shashank Shekhar <shashekh@iu.edu>
2018-12-17 19:21:22 -0500

    added text from ipsum, removed height manipulation of visualization view

--- projects/dvl-fw/src/lib/plugins/ngx-dino/visualizations/scatterplot-visualization.ts
+++ projects/dvl-fw/src/lib/plugins/ngx-dino/visualizations/scatterplot-visualization.ts
@@ -8,9 +8,19 @@ import { ScatterplotComponent } from '../components/scatterplot/scatterplot.comp
 
 export class ScatterplotVisualization extends DefaultVisualization {
   // TODO remove below string
-  readonly description = `He determined to drop his litigation with the monastry, and relinguish
-   his claims to the wood-cuting and fishery rihgts at once. He was the more ready to do this becuase the
-    rights had becom much less valuable, and he had indeed the vaguest idea where the wood and river in quedtion were.`;
+  readonly description = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+   incididunt ut labore et dolore magna aliqua. 
+  Dui accumsan sit amet nulla facilisi. Neque volutpat ac tincidunt vitae semper quis lectus nulla at.
+  Enim ut sem viverra aliquet eget sit. Vehicula ipsum a arcu cursus vitae congue mauris. Urna et pharetra
+   pharetra massa massa. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. At ultrices mi
+    tempus imperdiet nulla malesuada pellentesque elit eget. Sagittis nisl rhoncus mattis
+     rhoncus urna neque viverra justo. Urna cursus eget nunc scelerisque viverra
+     mauris in. Auctor urna nunc id cursus metus aliquam. Sed vulputate mi sit
+      amet mauris. Dolor magna eget est lorem ipsum dolor sit amet consectetur. Pretium aenean
+      pharetra magna ac placerat vestibulum. Turpis cursus in hac habitasse platea dictumst
+       quisque sagittis. Adipiscing vitae proin sagittis nisl rhoncus mattis
+       rhoncus urna. Massa placerat duis ultricies lacus sed. Orci eu lobortis
+        elementum nibh tellus. Ac turpis egestas maecenas pharetra convallis posuere morbi leo.`;
   readonly component = ScatterplotComponent;
   readonly graphicSymbolOptions = [{
     id: 'points', label: 'Points', type: 'area',
