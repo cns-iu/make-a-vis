9b52af6500f13aa76eeee01e089bcc1fea84e4bd
Bruce Herr II <bherr@indiana.edu>
2019-01-10 09:39:50 -0500

    Re-enable transparency

--- projects/dvl-fw/src/lib/encoding/common.ts
+++ projects/dvl-fw/src/lib/encoding/common.ts
@@ -55,7 +55,7 @@ export const strokeWidthScaleNormQuantitative = strokeWidthScale.quantitative([0
 export const quantitativeTransparency = map<number, number>(n => ((100 - n) / 100) * 0.49);
 
 export const defaultStyles = {
-  transparency: 0,
+  transparency: 0.25,
   strokeTransparency: 0.0,
   strokeWidth: 0.5,
   strokeColor: '#000007',
