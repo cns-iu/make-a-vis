e52321f53c5d58d58a96e5e7dd5c17eebd6a6ba3
axdanbol <daniel_bolin@yahoo.com>
2019-03-15 09:57:15 -0400

    Table export service tests

--- projects/make-a-vis/src/lib/data-view/shared/export-table.service.spec.ts
+++ projects/make-a-vis/src/lib/data-view/shared/export-table.service.spec.ts
@@ -1,12 +1,43 @@
 import { TestBed } from '@angular/core/testing';
+import * as fileSaver from 'file-saver';
+import * as papaparse from 'papaparse';
 
+import { DataSource } from './data.service';
 import { ExportTableService } from './export-table.service';
 
 describe('ExportTableService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  const dataSource = {
+    columns: [{ id: 'a', label: 'foo' }, { id: 'b', label: 'bar' }],
+    data: [{ a: 1, b: 2 }, { a: 3, b: 4 }]
+  } as DataSource;
+
+  let service: ExportTableService;
+  let saveAsSpy: jasmine.Spy;
+  let unparseSpy: jasmine.Spy;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+  });
+
+  beforeEach(() => {
+    service = TestBed.get(ExportTableService);
+    saveAsSpy = spyOn(fileSaver, 'saveAs');
+    unparseSpy = spyOn(papaparse, 'unparse').and.returnValue('');
+  });
+
+  beforeEach(() => {
+    service.save(dataSource);
+  });
 
   it('should be created', () => {
-    const service: ExportTableService = TestBed.get(ExportTableService);
     expect(service).toBeTruthy();
+  });
+
+  it('calls unparse to turn data into csv', () => {
+    expect(unparseSpy).toHaveBeenCalled();
+  });
+
+  it('calls saveAs to save the csv to file', () => {
+    expect(saveAsSpy).toHaveBeenCalled();
   });
 });
