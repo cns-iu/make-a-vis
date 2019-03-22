be394b80a11381dfaaae1fe8e73e40410d73c16a
Gagandeep Singh <singhgag@indiana.edu>
2019-03-22 15:03:22 -0400

    documentation for store in mav-selection

--- projects/make-a-vis/src/lib/mav-selection/shared/store/state.ts
+++ projects/make-a-vis/src/lib/mav-selection/shared/store/state.ts
@@ -1,13 +1,23 @@
+/**
+ * Graphic-variable-group panel state type
+ */
 export interface GVGroupPanelState {
   gsId: string;
   streamId: string;
 }
 
+
+/**
+ * Mav selection state type
+ */
 export interface MavSelectionState {
   gvPanelOpen: boolean;
   openedGVGroups: GVGroupPanelState[];
 }
 
+/**
+ * initial MavSelectionState
+ */
 export const initialMavSelectionState: MavSelectionState = {
   gvPanelOpen: false,
   openedGVGroups: []
