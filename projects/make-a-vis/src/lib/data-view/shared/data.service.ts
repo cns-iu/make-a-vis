f4066433c9ff52b4b66d7eb7606bf31a6e566d40
Gagandeep Singh <singhgag@indiana.edu>
2019-06-03 16:48:26 -0400

    functionality for external data referenced in a project + functionality to allow streaming data + temporary css for fixed row and cell height (WIP)

--- projects/make-a-vis/src/lib/data-view/shared/data.service.ts
+++ projects/make-a-vis/src/lib/data-view/shared/data.service.ts
@@ -1,13 +1,17 @@
 // refer https://angular.io/guide/styleguide#style-03-06 for import line spacing
 import { Injectable } from '@angular/core';
-import { select, Store} from '@ngrx/store';
-import { access, combine, Operator, RawChangeSet} from '@ngx-dino/core';
+import { DataVariable, GraphicVariable, Project, RecordSet } from '@dvl-fw/core';
+import { select, Store } from '@ngrx/store';
+import { access, combine, idSymbol, Operator, RawChangeSet } from '@ngx-dino/core';
 import { get } from 'lodash';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-import { DataVariable, GraphicVariable, Project, RecordSet } from '@dvl-fw/core';
 import { ApplicationState, getLoadedProject } from '../../shared/store';
 
+/**
+ * Data source interface
+ */
 export interface DataSource {
   id: string;
   label: string;
@@ -16,7 +20,7 @@ export interface DataSource {
   children: DataSource[];
   childrenHidden: boolean;
   columns: DataVariable[];
-  data: any[];
+  data: Observable<any[]>;
   level: number;
   hidden: boolean;
   hiddenData: boolean;
@@ -28,17 +32,29 @@ export interface DataSource {
   providedIn: 'root'
 })
 export class DataService {
+  /**
+   * Behavior subject to emit an updated data source
+   */
   private dataSourcesChange = new BehaviorSubject<DataSource[]>(undefined);
+  /**
+   * Observable from the dataSourcesChange BehaviorSubject to emit an updated data-source
+   */
   dataSourcesChanged: Observable<DataSource[]> = this.dataSourcesChange.asObservable();
+  /**
+   * Data sources array to hold all data sources
+   */
   dataSources: DataSource[];
 
+  /**
+   * Creates an instance of data service.
+   * @param store instance of Store which provides access to the ApplicationState
+   */
   constructor(private store: Store<ApplicationState>) {
     store.pipe(select(getLoadedProject))
       .subscribe((project: Project) => {
         if (project && project.recordSets) {
           this.dataSources = project.recordSets.map((recordSet: RecordSet) => {
             const dataSource: DataSource = {} as DataSource;
-
             dataSource.id = recordSet.id || '';
             dataSource.label = recordSet.label || '';
             dataSource.description = recordSet.description || undefined;
@@ -50,18 +66,25 @@ export class DataService {
             dataSource.hiddenData = false;
             dataSource.numRows = 0;
             dataSource.streamId = get(recordSet.defaultRecordStream, 'id');
+
             const operator = this.getDataMappingOperator(recordSet.dataVariables, project.graphicVariables, recordSet.id);
             if (recordSet.defaultRecordStream) {
-              recordSet.defaultRecordStream.asObservable().subscribe((changeSet: RawChangeSet<any>) => {
-                dataSource.data = (changeSet.insert || []).map(operator.getter);
-                dataSource.numRows += (changeSet.insert.length || 0) - (changeSet.remove.length || 0);
-                if (changeSet.insert.length > 1) {
-                  dataSource.label = recordSet.labelPlural;
-                }
-              });
+              const data: any[] = [];
+              dataSource.data = recordSet.defaultRecordStream.asObservable().pipe(
+                map((changeSet: RawChangeSet<any>) => {
+                  dataSource.numRows += (changeSet.insert.length || 0) - (changeSet.remove.length || 0);
+
+                  if (changeSet.insert.length > 1) {
+                    dataSource.label = recordSet.labelPlural;
+                  }
+
+                  return this.updateData(data, changeSet).map(operator.getter);
+                }));
             }
+
             return dataSource;
           });
+
           this.dataSources = this.getFlatArrayOfDataSources(this.getDataSourceWithChildren(this.dataSources, 0));
           this.dataSourcesChange.next(this.dataSources);
         } else {
@@ -70,6 +93,38 @@ export class DataService {
       });
   }
 
+  private updateData(data: any[], changeSet: RawChangeSet<any>): any[] {
+    if (changeSet.remove.length) {
+      const filtered = data.filter(e => {
+        return !changeSet.remove.some(obj => obj[idSymbol] === e.id);
+      });
+      data = filtered.concat(changeSet.insert);
+    } else if (changeSet.insert.length) {
+      data = data.concat(changeSet.insert);
+    }
+
+    if (changeSet.update.length) {
+      changeSet.update.forEach(el => {
+        const index = data.findIndex(e => e.id === el[idSymbol]);
+        data[index] = Object.assign(data[index] || {}, el[idSymbol]);
+      });
+    }
+
+    if (changeSet.replace.length) {
+      changeSet.replace.forEach(el => {
+        const index = data.findIndex(e => e.id === el[1].id);
+        data[index] = el;
+      });
+    }
+
+    return data;
+  }
+
+  /**
+   * Gets a flat array of data sources
+   * @param dataSources array of data-sources
+   * @returns flat array of data sources
+   */
   getFlatArrayOfDataSources(dataSources: DataSource[]): DataSource[] {
     const stack = dataSources;
     const flastDataSources: DataSource[] = [];
@@ -77,11 +132,18 @@ export class DataService {
       const dataSource = stack.shift();
       flastDataSources.push(dataSource);
       const children = dataSource.children;
-      [].unshift.apply(stack , children);
+      [].unshift.apply(stack, children);
     }
     return flastDataSources;
   }
 
+  /**
+   * Constructs a hierarchy of data-sources
+   * @param dataSources array of data-sources
+   * @param id id of data-source
+   * @param level level of hierarchy
+   * @returns an array of data-sources with hierarchical information
+   */
   getChildrenForRecord(dataSources: DataSource[], id: string, level: number): DataSource[] {
     const childrenList = [];
     dataSources.forEach(element => {
@@ -94,10 +156,16 @@ export class DataService {
     return childrenList;
   }
 
+  /**
+   * Gets data sources with info on their children
+   * @param dataSources array of data-sources
+   * @param level level of hierarchy in the hierarchy of data-sources
+   * @returns array of data-sources with their children information
+   */
   getDataSourceWithChildren(dataSources: DataSource[], level: number): DataSource[] {
     const dataSourceWithChildren: DataSource[] = [];
     for (const dataSource of dataSources) {
-      if ( dataSource.parent === undefined) {
+      if (dataSource.parent === undefined) {
         dataSource.children = this.getChildrenForRecord(dataSources, dataSource.id, level);
         dataSourceWithChildren.push(dataSource);
       }
@@ -105,6 +173,10 @@ export class DataService {
     return dataSourceWithChildren;
   }
 
+  /**
+   * Toggles the children of a data source
+   * @param children array of children data-sources
+   */
   toggleChildren(children: DataSource[]) {
     for (const child of children) {
       child.hidden = !child.hidden;
@@ -112,17 +184,28 @@ export class DataService {
     }
   }
 
+  /**
+   * Toggles the data table
+   * @param dataSource the data-source associated with the data-table
+   */
   toggleDataTable(dataSource: DataSource) {
     dataSource.childrenHidden = !dataSource.childrenHidden;
     this.toggleChildren(dataSource.children);
   }
 
+  /**
+   * Gets data mapping operator
+   * @param dataVariables array of data-variables
+   * @param graphicVariables array of graphic-variables
+   * @param recordSetId record-set-id associated with
+   * @returns data variable to graphic variable mapping operator
+   */
   getDataMappingOperator(dataVariables: DataVariable[], graphicVariables: GraphicVariable[], recordSetId: string): Operator<any, any> {
     const mapping = {};
     dataVariables.forEach((dv: DataVariable) => {
       const filter = graphicVariables.filter((gv: GraphicVariable) => {
         if ((gv.dataVariable.id === dv.id) && (gv.recordSet.id === recordSetId)
-          && (gv.type === 'text' || gv.type === 'label') ) {
+          && (gv.type === 'text' || gv.type === 'label')) {
           return gv;
         }
       });
--- projects/make-a-vis/src/lib/data-view/shared/export-table.service.ts
+++ projects/make-a-vis/src/lib/data-view/shared/export-table.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { saveAs } from 'file-saver';
 import { at as loAt, map as loMap } from 'lodash';
 import { unparse } from 'papaparse';
 
 import { DataSource } from './data.service';
+import { Subscription } from 'rxjs';
 
 /**
  * Service for saving a table's data to a csv file.
@@ -11,18 +12,34 @@ import { DataSource } from './data.service';
 @Injectable({
   providedIn: 'root'
 })
-export class ExportTableService {
+export class ExportTableService implements OnDestroy {
+  /**
+   * Data subscription of export table service
+   */
+  private dataSubscription: Subscription;
+
   /**
    * Saves table data to a csv file.
    *
    * @param source The table data.
    */
-  save(source: DataSource): void {
-    const header = loMap(source.columns, 'label');
-    const ids = loMap(source.columns, 'id');
-    const values = loMap(source.data, item => loAt(item, ids));
-    const csv = unparse({ fields: header, data: values });
-    const blob = new Blob([csv], { type: 'text/csv' });
-    saveAs(blob, source.label + '.csv');
+   save(source: DataSource): void {
+    this.dataSubscription = source.data.subscribe((data) => {
+      const header = loMap(source.columns, 'label');
+      const ids = loMap(source.columns, 'id');
+      const values = loMap(data, item => loAt(item, ids));
+      const csv = unparse({ fields: header, data: values });
+      const blob = new Blob([csv], { type: 'text/csv' });
+      saveAs(blob, source.label + '.csv');
+    });
+  }
+
+  /**
+   * on destroy life cycle hook
+   */
+  ngOnDestroy() {
+    if (this.dataSubscription) {
+      this.dataSubscription.unsubscribe();
+    }
   }
 }
