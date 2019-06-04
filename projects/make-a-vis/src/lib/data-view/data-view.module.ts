605c094aa4b1da862d79be96b1e659491d4a7e6d
Gagandeep Singh <singhgag@indiana.edu>
2019-06-04 16:39:48 -0400

    added styling to make long texts into an ellipsis in the table in the data-view + minor console.log to console.error change

--- projects/make-a-vis/src/lib/data-view/data-view.module.ts
+++ projects/make-a-vis/src/lib/data-view/data-view.module.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
