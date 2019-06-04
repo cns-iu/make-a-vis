45d91156ef2702e4fd7db288384afa3a7a44935d
axdanbol <daniel_bolin@yahoo.com>
2019-06-04 13:29:08 -0400

    Register angular components as web components

--- projects/mav-embed/src/app/app.module.ts
+++ projects/mav-embed/src/app/app.module.ts
@@ -1,25 +1,41 @@
-import { DoBootstrap, NgModule, Injector } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { DoBootstrap, Injector, NgModule, Type } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
+import { DvlFwModule } from '@dvl-fw/core';
 
-import { ProjectComponent } from './project/project.component';
 import { LegendComponent } from './legend/legend.component';
+import { ProjectComponent } from './project/project.component';
 import { VisualizationComponent } from './visualization/visualization.component';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    DvlFwModule
   ],
   declarations: [
-    ProjectComponent,
     LegendComponent,
+    ProjectComponent,
     VisualizationComponent
   ],
-  entryComponents: [ProjectComponent, LegendComponent, VisualizationComponent]
+  entryComponents: [
+    LegendComponent,
+    ProjectComponent,
+    VisualizationComponent
+  ]
 })
 export class AppModule implements DoBootstrap {
   constructor(injector: Injector) {
-    // TODO
+    this.registerCustomElement('mav-legend', LegendComponent, injector);
+    this.registerCustomElement('mav-project', ProjectComponent, injector);
+    this.registerCustomElement('mav-visualization', VisualizationComponent, injector);
   }
 
   ngDoBootstrap() { }
+
+  private registerCustomElement<T>(selector: string, component: Type<T>, injector: Injector): void {
+    const element = createCustomElement(component, { injector });
+    customElements.define(selector, element);
+  }
 }
