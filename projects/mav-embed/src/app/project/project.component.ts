d5c546553a8a61c563069a42424af5773e4180c3
axdanbol <daniel_bolin@yahoo.com>
2019-06-07 10:17:26 -0400

    Added documentation for mav-project

--- projects/mav-embed/src/app/project/project.component.ts
+++ projects/mav-embed/src/app/project/project.component.ts
@@ -6,21 +6,39 @@ import { DataLinkService, DataLinkSource } from '../shared/services/data-link/da
 import { SourceType } from '../shared/services/project-loader/project-loader.service';
 import { ProjectManagerService } from '../shared/services/project-manager/project-manager.service';
 
+/**
+ * A component for loading and referencing a dvl-fw project
+ */
 @Component({
   selector: 'mav-project',
   template: '',
   encapsulation: ViewEncapsulation.ShadowDom
 })
 export class ProjectComponent implements DataLinkSource<Project>, OnChanges, OnInit, OnDestroy {
+  /** The identifier for this project */
   @Input() id: string;
+  /** The name of a global variable containing a project ot project yaml */
   @Input() data: string;
+  /** The url to the project yaml */
   @Input() href: string;
 
+  /** The project data */
   data$: Observable<Project> = EMPTY;
+  /** An observable emitting when data changes */
   change$ = new BehaviorSubject<any>(undefined);
 
+  /**
+   * Creates an instance of project component.
+   * @param linker The data linker
+   * @param manager The project manager
+   */
   constructor(private linker: DataLinkService, private manager: ProjectManagerService) { }
 
+  /**
+   * Angular's on change hook
+   * Detext changes to id, data, and href
+   * @param changes The changed values
+   */
   ngOnChanges(changes: SimpleChanges) {
     const globalChanged = 'data' in changes;
     const hrefChanged = 'href' in changes;
@@ -39,17 +57,30 @@ export class ProjectComponent implements DataLinkSource<Project>, OnChanges, OnI
     }
   }
 
+  /**
+   * Angular's on init hook
+   */
   ngOnInit() {
     this.linker.registerSource(this);
   }
 
+  /**
+   * Angular's on destroy hook
+   * Cleans up subscriptions
+   */
   ngOnDestroy() {
     this.linker.unregisterSource(this);
     this.unsetData();
   }
 
+  /** Emits a change */
   private changed(): void { this.change$.next(undefined); }
 
+  /**
+   * Sets the data observable
+   * @param [global] The global data name
+   * @param [href] The data url
+   */
   private setData(global = this.data, href = this.href): void {
     if (!global !== !href) { // XOR operation ;)
       const source = global || href;
@@ -67,6 +98,11 @@ export class ProjectComponent implements DataLinkSource<Project>, OnChanges, OnI
     }
   }
 
+  /**
+   * Unsets the data observable
+   * @param [global] The global data name
+   * @param [href] The data url
+   */
   private unsetData(global = this.data, href = this.href): void {
     if (!global !== !href) { // XOR operation ;)
       const source = global || href;
