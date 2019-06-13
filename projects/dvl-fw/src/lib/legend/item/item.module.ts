e76c25ee39e96bb7abdad117c61d163ffe7d5815
axdanbol <daniel_bolin@yahoo.com>
2019-06-13 14:22:04 -0400

    Implemented legend item

--- projects/dvl-fw/src/lib/legend/item/item.module.ts
+++ projects/dvl-fw/src/lib/legend/item/item.module.ts
@@ -1,11 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
+import { VisualizationModule } from '../../visualization/visualization.module';
 import { ItemComponent } from '../item/item.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    VisualizationModule
   ],
   declarations: [ItemComponent],
   exports: [ItemComponent]
