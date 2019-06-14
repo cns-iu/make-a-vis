a5b4e8567bdcaf0679f5993400d34712290107ae
Gagandeep Singh <singhgag@indiana.edu>
2019-06-14 12:51:53 -0400

    minor cleanup and documentation

--- projects/make-a-vis/src/lib/data-view/start-project-options/start-project-options.component.ts
+++ projects/make-a-vis/src/lib/data-view/start-project-options/start-project-options.component.ts
@@ -15,26 +15,30 @@ export class StartProjectOptionsComponent {
 
   constructor(public loadProjectService: LoadProjectService, public snackBar: MatSnackBar) { }
 
-  readNewFile(event: any, target: any, selectedExtension: ProjectExtensionType) {
+  /**
+  * Reads new file/s
+  * @param event the onload event object
+  * @param selectedExtension the intended extension of files
+  */
+  readNewFile(event: any, selectedExtension: ProjectExtensionType) {
     const numFiles = get(event, 'srcElement.files.length');
     let areValidExtensions = false;
     const filenames: string[] = [];
 
     for (let i = 0; i < numFiles; i++) {
       const filename = get(event, 'srcElement.files[' + i + '].name');
-      filenames.push(filename);
 
       if (!filename) {
         return;
       }
 
+      filenames.push(filename);
       const fileExtension = filename && filename.split('.').slice(-1).toString().toLowerCase();
       if (this.isValidFileExtension(selectedExtension, fileExtension)) {
         areValidExtensions = true;
       } else {
         areValidExtensions = false;
         // TODO temporary, use logs
-        // alert(`${filename} has the wrong extension.`);
         this.snackBar.open(`${filename} has the wrong extension.`, null, {
           duration: 3000,
           verticalPosition: 'top',
@@ -49,7 +53,7 @@ export class StartProjectOptionsComponent {
       this.loadProjectService.getProject(filenames, selectedExtension, event);
     }
 
-    //  clear the values of fileinput tags.
+    //  clear the values of file input tags.
     if (this.fileInputTags) {
       this.fileInputTags.forEach((elementRef: ElementRef) => {
         elementRef.nativeElement.value = null;
@@ -57,6 +61,12 @@ export class StartProjectOptionsComponent {
     }
   }
 
+  /**
+  * Determines whether the file extension of the file uploaded is the same as the intended extension
+  * @param selectedExtension selected file extension
+  * @param fileExtension the uploaded file's extension
+  * @returns true if valid file extension
+  */
   isValidFileExtension(selectedExtensionOnButton: ProjectExtensionType, fileExtensionFromFile: ProjectExtensionType): boolean {
     return this.loadProjectService.getSupportedExtension(selectedExtensionOnButton).split(',').indexOf('.' + fileExtensionFromFile) !== -1;
   }
