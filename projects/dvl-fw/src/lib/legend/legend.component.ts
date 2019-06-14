9df880de6d51a1c775761a69d53033ac450c0bcc
axdanbol <daniel_bolin@yahoo.com>
2019-06-14 12:26:15 -0400

    Implemented dvl-legend

--- projects/dvl-fw/src/lib/legend/legend.component.ts
+++ projects/dvl-fw/src/lib/legend/legend.component.ts
@@ -1,24 +1,67 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
+import { castArray, every, filter, map, propertyOf, some } from 'lodash';
 
-import { Visualization } from '../shared/visualization';
+import { Project } from '../shared/project';
+import { GraphicSymbolOption, Visualization } from '../shared/visualization';
+import { LegendType } from './item/item.component';
 
-export type LegendType = 'dynamic-only' | 'static-only' | 'dynamic-preferred' | 'static-preferred';
 export type LegendOrientation = 'horizontal' | 'vertical';
+
+interface ItemGroup extends Array<string> {
+  id: string;
+  label: string;
+}
 
 @Component({
   selector: 'dvl-legend',
   templateUrl: './legend.component.html',
   styleUrls: ['./legend.component.scss']
 })
-export class LegendComponent implements OnInit {
+export class LegendComponent implements OnChanges, OnDestroy {
+  @Input() project: Project;
   @Input() visualization: Visualization;
-  @Input() symbolGroups: string | string[] = undefined;
-  @Input() type: LegendType = 'dynamic-preferred';
-  @Input() orientation: LegendOrientation = 'horizontal';
+  @Input() symbolGroups: string | string[];
+  @Input() orientation: LegendOrientation;
+  @Input() type: LegendType;
+  @Input() advanced: boolean;
+
+  groups: ItemGroup[] = [];
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges) {
+    const { project, visualization } = this;
+    const checkedValues = [project, visualization];
+    const checkedProperties = ['visualization', 'symbolGroups'];
+
+    if (!every(checkedValues)) {
+      this.clear();
+    } else if (some(checkedProperties, propertyOf(changes))) {
+      this.update();
+    }
   }
 
+  ngOnDestroy() {
+    this.clear();
+  }
+
+  private update(): void {
+    const { visualization: { graphicSymbolOptions: symbolOpts }, symbolGroups } = this;
+    const ids = new Set(symbolGroups === undefined ? map(symbolOpts, 'id') : castArray(symbolGroups));
+    const filteredOpts = filter(symbolOpts, ({ id }) => ids.has(id));
+    const groups = map(filteredOpts, this.createGroup);
+    const filteredGroups = filter(groups, 'length');
+
+    this.groups = filteredGroups;
+  }
+
+  private clear(): void {
+    this.groups = [];
+  }
+
+  private createGroup(this: void, symbolOpt: GraphicSymbolOption): ItemGroup {
+    const { id, label, graphicVariableOptions } = symbolOpt;
+    const items = filter(map(graphicVariableOptions, 'id'));
+    return Object.assign(items, { id, label });
+  }
 }
--- projects/dvl-fw/src/lib/legend/legend.module.ts
+++ projects/dvl-fw/src/lib/legend/legend.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 import { ItemModule } from './item/item.module';
 import { LegendComponent } from './legend.component';
@@ -7,6 +8,7 @@ import { LegendComponent } from './legend.component';
 @NgModule({
   imports: [
     CommonModule,
+    MatCardModule,
     ItemModule
   ],
   declarations: [LegendComponent],
