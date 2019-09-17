bbebd51b7a19330824ae0558ae0ae277348f6550
Bruce Herr II <bherr@indiana.edu>
2019-09-17 13:22:31 -0400

    Re-added the old type inferencing (with some minor updates to fit into the new code).

--- projects/dvl-fw/src/lib/shared/csv-parser.ts
+++ projects/dvl-fw/src/lib/shared/csv-parser.ts
@@ -1,13 +1,8 @@
-import {
-  parse as papaparse,
-  ParseConfig,
-  ParseError,
-  ParseMeta as PapaParseMeta,
-  ParseResult as PapaParseResult,
-} from 'papaparse';
-import { times } from 'lodash';
+import { isBoolean, isInteger, isNumber } from 'lodash';
+import { parse as papaparse, ParseConfig, ParseError, ParseMeta as PapaParseMeta, ParseResult as PapaParseResult } from 'papaparse';
 
 import { DataType } from './data-variable';
+
 
 export { ParseConfig, ParseError };
 export interface ParseMeta extends PapaParseMeta {
@@ -22,7 +17,28 @@ export function parse(input: string, config: ParseConfig): ParseResult {
   const result = papaparse(input, config) as ParseResult;
   const fields = result.meta.fields;
   if (fields) {
-    result.meta.typings = times(fields.length, () => DataType.STRING);
+    const types: { [field: string]: DataType } = {};
+
+    if (config.dynamicTyping) {
+      // Add better type inferencing later
+      result.data.forEach(item => {
+        for (const key of fields) {
+          const value = item[key];
+          if (isInteger(value)) {
+            types[key] = DataType.INTEGER;
+          } else if (isNumber(value)) {
+            types[key] = DataType.NUMBER;
+          } else if (isBoolean(value)) {
+            types[key] = DataType.STRING;
+            item[key] = value ? 'true' : 'false';
+          } else if (!types.hasOwnProperty(key)) {
+            types[key] = DataType.STRING;
+          }
+        }
+      });
+    }
+
+    result.meta.typings = fields.map(f => types[f] || DataType.STRING);
   }
 
   return result;
