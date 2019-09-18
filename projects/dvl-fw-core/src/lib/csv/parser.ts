ffe215865dfb99013403efe5279ca6dee853b02b
axdanbol <daniel_bolin@yahoo.com>
2019-09-18 15:51:38 -0400

    Added basic type inference to csv parser

--- projects/dvl-fw-core/src/lib/csv/parser.ts
+++ projects/dvl-fw-core/src/lib/csv/parser.ts
@@ -1,4 +1,4 @@
-import { times } from 'lodash';
+import { isBoolean, isInteger, isNumber } from 'lodash';
 import { parse as baseParse, ParseConfig, ParseError, ParseMeta as BaseMeta, ParseResult as BaseResult } from 'papaparse';
 
 import { DataType } from '../interfaces';
@@ -16,7 +16,28 @@ export function parse(input: string, config: ParseConfig): ParseResult {
   const result = baseParse(input, config) as ParseResult;
   const fields = result.meta.fields;
   if (fields) {
-    result.meta.typings = times(fields.length, () => DataType.STRING);
+    const types: { [field: string]: DataType } = {};
+
+    if (config.dynamicTyping) {
+      // Add better type inferencing later
+      result.data.forEach(item => {
+        for (const key of fields) {
+          const value = item[key];
+          if (isInteger(value)) {
+            types[key] = DataType.INTEGER;
+          } else if (isNumber(value)) {
+            types[key] = DataType.NUMBER;
+          } else if (isBoolean(value)) {
+            types[key] = DataType.STRING;
+            item[key] = value ? 'true' : 'false';
+          } else if (!types.hasOwnProperty(key)) {
+            types[key] = DataType.STRING;
+          }
+        }
+      });
+    }
+
+    result.meta.typings = fields.map(f => types[f] || DataType.STRING);
   }
 
   return result;
