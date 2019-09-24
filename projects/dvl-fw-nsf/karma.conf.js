6fe1ddc8d63e71d3cff9cb12cf22cd19cdfdb5bf
axdanbol <daniel_bolin@yahoo.com>
2019-09-24 11:17:29 -0400

    Added nsf scaffolding

--- projects/dvl-fw-nsf/karma.conf.js
+++ projects/dvl-fw-nsf/karma.conf.js
@@ -4,19 +4,18 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage/dvl-fw-ngx-dino'),
+      dir: require('path').join(__dirname, '../../coverage/dvl-fw-nsf'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
       thresholds: {
