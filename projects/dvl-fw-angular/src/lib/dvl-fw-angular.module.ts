912f30a2662a92470145b35fc0bfd1588c82999b
axdanbol <daniel_bolin@yahoo.com>
2019-09-25 12:10:05 -0400

    Fixed some exports

--- projects/dvl-fw-angular/src/lib/dvl-fw-angular.module.ts
+++ projects/dvl-fw-angular/src/lib/dvl-fw-angular.module.ts
@@ -4,6 +4,7 @@ import { LegendModule } from './legend/legend.module';
 import { VisualizationModule } from './visualization/visualization.module';
 
 @NgModule({
-  imports: [LegendModule, VisualizationModule]
+  imports: [LegendModule, VisualizationModule],
+  exports: [LegendModule, VisualizationModule]
 })
 export class DvlFwAngularModule {}
--- projects/dvl-fw-core/src/lib/interfaces/record-stream.ts
+++ projects/dvl-fw-core/src/lib/interfaces/record-stream.ts
@@ -1,10 +1,10 @@
 import { RawChangeSet } from '@ngx-dino/core';
-import { InteropObservable, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
 export interface RecordStream<T = any> {
   id: string;
   label: string;
 
-  asObservable(): Observable<RawChangeSet<T>> | InteropObservable<RawChangeSet<T>>;
+  asObservable(): Observable<RawChangeSet<T>>;
   toJSON(): any;
 }
