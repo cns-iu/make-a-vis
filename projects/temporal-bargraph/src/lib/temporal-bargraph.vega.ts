2115d8c6ac4131cb81c5e53183c0d731d9503098
axdanbol <daniel_bolin@yahoo.com>
2020-07-09 14:32:27 -0400

    Improved temporal bargraph

--- projects/temporal-bargraph/src/lib/temporal-bargraph.vega.ts
+++ projects/temporal-bargraph/src/lib/temporal-bargraph.vega.ts
@@ -11,47 +11,94 @@ export function temporalBargraphSpec(options: TemporalBargraphSpecOptions = {}):
   return {
     '$schema': 'https://vega.github.io/schema/vega-lite/v4.json',
     description: 'A temporal bar graph is a visualization where each record is presented as a labeled horizontal bar with a specific start and end date. The area of each bar encodes a numerical attribute value (e.g., total amount of funding). Bars may be color coded to present categorical attribute values of records. An additional variable can be presented through tooltips on the bars.',
-    autosize: {type: 'fit', resize: true},
+    autosize: {
+      type: 'fit',
+      resize: true
+    },
     width: 'container',
     height: 'container',
-    config: {view: {strokeOpacity: 0}},
+    config: {
+      view: {
+        strokeOpacity: 0
+      }
+    },
+
+    data: {
+      name: 'nodes'
+    },
+
+    transform: [
+      {
+        calculate: `'' + datum['y-order'] + '-' + datum.identifier`,
+        as: 'order'
+      },
+      {
+        calculate: `!isValid(datum.tooltip) ? '' : datum.tooltip`,
+        as: 'tooltip'
+      },
+      {
+        calculate: '!isValid(datum.strokeWidth) ? 1 : datum.strokeWidth',
+        as: 'strokeWidth'
+      },
+      {
+        calculate: '!isValid(datum.strokeTransparency) ? 0.9 : 1 - datum.strokeTransparency',
+        as: 'strokeOpacity'
+      },
+      {
+        calculate: '!isValid(datum.transparency) ? 0.75 : 1 - datum.transparency',
+        as: 'opacity'
+      },
+    ],
+
+    encoding: {
+      x: { field: 'x-start', type: 'ordinal' },
+      y: {
+        field: 'order',
+        type: 'ordinal',
+        sort: '-x',
+        axis: null
+      }
+    },
+
     layer: [
-      // Draw nodes
+      // Bars
       {
         mark: 'bar',
-        data: {name: 'nodes', values: options.nodes as any[] || undefined},
-        transform: [
-          {
-            calculate: '!isValid(datum.tooltip) ? \'\' : datum.tooltip',
-            as: 'tooltip'
-          },
-          {
-            calculate: '!isValid(datum.strokeWidth) ? 1 : datum.strokeWidth',
-            as: 'strokeWidth'
-          },
-          {
-            calculate: '!isValid(datum.strokeTransparency) ? 0.9 : 1 - datum.strokeTransparency',
-            as: 'strokeOpacity'
-          },
-          {
-            calculate: '!isValid(datum.transparency) ? 0.75 : 1 - datum.transparency',
-            as: 'opacity'
-          },
-        ],
         encoding: {
-          x: {field: 'x-start', type: 'ordinal'},
-          x2: {field: 'x-end', type: 'ordinal'},
-          y: {field: 'y-order', type: 'quantitative'},
-          shape: {field: 'shape', type: 'nominal', scale: null},
-          fill: {field: 'color', type: 'nominal', scale: null},
-          fillOpacity: {field: 'opacity', type: 'quantitative', scale: null},
-          stroke: {field: 'strokeColor', type: 'nominal', scale: null},
-          strokeWidth: {field: 'strokeWidth', type: 'quantitative', scale: null},
-          strokeOpacity: {field: 'strokeOpacity', type: 'quantitative', scale: null},
-          size: {field: 'areaSize', type: 'quantitative', scale: null},
-          tooltip: {field: 'tooltip', type: 'nominal'}
+          x2: { field: 'x-end' },
+          fill: { field: 'color', type: 'nominal', scale: null },
+          fillOpacity: { field: 'opacity', type: 'quantitative', scale: null },
+          stroke: { field: 'strokeColor', type: 'nominal', scale: null },
+          strokeWidth: { field: 'strokeWidth', type: 'quantitative', scale: null },
+          strokeOpacity: { field: 'strokeOpacity', type: 'quantitative', scale: null },
+          size: { field: 'areaSize', type: 'quantitative' /*, scale: null */ },
+          tooltip: { field: 'tooltip', type: 'nominal' }
+        }
+      },
+      // Labels
+      {
+        mark: {
+          type: 'text',
+          align: 'right',
+          baseline: 'middle',
+          dx: -3,
+          limit: 150,
+          fontSize: 7
+        },
+        encoding: {
+          text: {
+            field: 'label',
+            type: 'nominal'
+          },
+          tooltip: {
+            field: 'label',
+            type: 'nominal'
+          }
         }
       }
-    ]
+    ],
+    datasets: {
+      'nodes': options.nodes as any[]
+    }
   };
 }
