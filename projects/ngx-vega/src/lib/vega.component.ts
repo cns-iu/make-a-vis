7fae1cacee52e5723f7604a150046bb684e2ed9e
axdanbol <daniel_bolin@yahoo.com>
2021-01-13 09:20:10 -0500

    Updated ngx-vega to better handle resizing and view changes

--- projects/ngx-vega/src/lib/vega.component.ts
+++ projects/ngx-vega/src/lib/vega.component.ts
@@ -1,16 +1,22 @@
 import {
-  ChangeDetectionStrategy, Component, ElementRef, Input, OnChanges, OnDestroy, Output, Renderer2, SimpleChanges,
+  ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, Input, OnChanges, OnDestroy, Output, Renderer2,
+  SimpleChanges,
 } from '@angular/core';
 import { ResizeSensor } from 'css-element-queries';
-import { Subscription } from 'rxjs';
 import { View } from 'vega';
 import { EmbedOptions, VisualizationSpec } from 'vega-embed';
 
-import { ViewManagerService } from './view-manager.service';
+import { ViewEvent, ViewManagerService } from './view-manager.service';
 
 
 export type Spec = VisualizationSpec | string;
 export type Options = EmbedOptions;
+export type AutosizeObj = Record<'width' | 'height', boolean>;
+export type Autosize = boolean | Partial<AutosizeObj>;
+
+
+function empty() {}
+
 
 @Component({
   selector: 'ngx-vega',
@@ -23,70 +29,130 @@ export class VegaComponent implements OnChanges, OnDestroy {
   @Input() spec: Spec;
   @Input() options?: Options;
 
+  @Input()
+  set autosize(value: Autosize) {
+    this._autosize = typeof value === 'boolean' ?
+      { width: value, height: value } :
+      { width: false, height: false, ...value };
+  }
+  get autosize(): Autosize {
+    return this._autosize;
+  }
+
+  private _autosize: AutosizeObj = {
+    // Default to true for backwards compatibility
+    width: true,
+    height: true
+  };
+
+  @Input() minSizeChange = 1;
+
   @Output() viewChange = this.manager.view$;
+  @Output() viewLoading = this.manager.loading$;
+  @Output() viewError = this.manager.error$;
 
   private readonly container: HTMLElement;
-  private viewEl?: HTMLElement;
-
-  private readonly subscriptions = new Subscription();
+  private readonly subscriptions = this.manager.event$.subscribe(this.handleEvents.bind(this));
+  private detach = empty;
 
   constructor(
     elementRef: ElementRef<HTMLElement>,
     private readonly manager: ViewManagerService,
-    private readonly renderer: Renderer2
+    private readonly renderer: Renderer2,
+    private readonly cdr: ChangeDetectorRef
   ) {
     this.container = elementRef.nativeElement;
-    this.subscriptions.add(manager.view$.subscribe(view => this.onViewChange(view)));
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     if ('spec' in changes || 'options' in changes) {
-      this.createView();
+      this.create();
     }
   }
 
   ngOnDestroy(): void {
-    this.clearContainer();
     this.subscriptions.unsubscribe();
+    this.detach();
   }
 
-  private onViewChange(view: View): void {
-    const { container, renderer } = this;
-    let viewEl = view.container();
-    let parentEl: HTMLElement = renderer.parentNode(viewEl);
-
-    // Find top wrapper element
-    while (parentEl !== null) {
-      viewEl = parentEl;
-      parentEl = renderer.parentNode(viewEl);
-    }
-
-    this.clearContainer();
-    this.viewEl = viewEl;
-
-    renderer.appendChild(container, viewEl);
-    // tslint:disable-next-line: no-unused-expression
-    new ResizeSensor(viewEl, ({width, height}) => {
-      view.width(width);
-      view.height(height);
-      view.runAsync();
-    });
-  }
-
-  private createView(): void {
+  private create(): void {
     const { spec, options, manager, renderer } = this;
     if (spec) {
       const viewEl = renderer.createElement('div') as HTMLElement;
       manager.create(viewEl, spec, options);
+    } else {
+      this.detach();
     }
   }
 
-  private clearContainer(): void {
-    const { container, viewEl, renderer } = this;
-    if (viewEl) {
-      ResizeSensor.detach(viewEl);
-      renderer.removeChild(container, viewEl);
-      this.viewEl = undefined;
+  private handleEvents(event: ViewEvent): void {
+    if (event.type === 'ready') {
+      this.attach(event.view);
+    } else if (event.type === 'error') {
+      this.detach();
     }
+  }
+
+  private attach(view: View): void {
+    const { cdr, container, renderer } = this;
+    const viewEl = this.getRoot(view.container());
+    const sensor = new ResizeSensor(viewEl, ({ width, height }) => {
+      this.updateSize(viewEl, view, width, height);
+    });
+
+    this.detach();
+    this.detach = () => {
+      sensor.detach();
+      renderer.removeChild(container, viewEl);
+      cdr.markForCheck();
+      this.detach = empty;
+    };
+
+    renderer.appendChild(container, viewEl);
+    cdr.markForCheck();
+  }
+
+  private updateSize(viewEl: HTMLElement, view: View, width: number, height: number): void {
+    const { _autosize: autosize } = this;
+    const widthChanged = autosize.width && this.updateWidth(viewEl, view, width);
+    const heightChanged = autosize.height && this.updateHeight(viewEl, view, height);
+
+    if (widthChanged || heightChanged) {
+      view.runAsync();
+    }
+  }
+
+  private updateWidth(viewEl: HTMLElement, view: View, width: number): boolean {
+    const { minSizeChange } = this;
+    const hasActions = viewEl.classList.contains('has-actions');
+    const padding = hasActions ? 38 : 0;
+    const diff = Math.abs(width - view.width() - padding);
+    if (diff >= minSizeChange) {
+      view.width(width - padding);
+      return true;
+    }
+    return false;
+  }
+
+  private updateHeight(_viewEl: HTMLElement, view: View, height: number): boolean {
+    const { minSizeChange } = this;
+    const diff = Math.abs(height - view.height());
+    if (diff >= minSizeChange) {
+      view.height(height);
+      return true;
+    }
+    return false;
+  }
+
+  private getRoot(el: HTMLElement): HTMLElement {
+    const { renderer } = this;
+    let parentEl = renderer.parentNode(el) as HTMLElement;
+
+    while (parentEl) {
+      el = parentEl;
+      parentEl = renderer.parentNode(parentEl);
+    }
+
+    return el;
   }
 }
--- projects/ngx-vega/src/lib/view-manager.service.ts
+++ projects/ngx-vega/src/lib/view-manager.service.ts
@@ -1,57 +1,73 @@
 import { Injectable, OnDestroy } from '@angular/core';
-import { ReplaySubject } from 'rxjs';
+import { from, Observable, ReplaySubject, Subscription } from 'rxjs';
+import { catchError, distinctUntilChanged, filter, map, pluck, shareReplay, startWith, switchAll } from 'rxjs/operators';
 import { View } from 'vega';
-import embed, { Result } from 'vega-embed';
+import embed from 'vega-embed';
+
+
+export interface ViewLoading {
+  type: 'loading';
+  loader: Promise<View>;
+}
+
+export interface ViewReady {
+  type: 'ready';
+  view: View;
+}
+
+export interface ViewError {
+  type: 'error';
+  error: unknown;
+}
+
+export type ViewEvent = ViewLoading | ViewReady | ViewError;
 
 
 @Injectable()
 export class ViewManagerService implements OnDestroy {
-  private readonly instances: Promise<Result>[] = [];
-  private readonly _view$ = new ReplaySubject<View>(1);
+  private readonly loader$ = new ReplaySubject<Observable<ViewEvent>>(1);
+  private subscriptions = new Subscription();
 
-  readonly view$ = this._view$.asObservable();
+  readonly event$ = this.loader$.pipe(
+    switchAll(),
+    shareReplay(1)
+  );
+
+  readonly view$ = this.event$.pipe(
+    filter((event): event is ViewReady => event.type === 'ready'),
+    pluck('view')
+  );
+
+  readonly loading$ = this.event$.pipe(
+    map(event => event.type === 'loading'),
+    distinctUntilChanged(),
+  );
+
+  readonly error$ = this.event$.pipe(
+    filter((event): event is ViewError => event.type === 'error'),
+    pluck('error')
+  );
 
   ngOnDestroy(): void {
-    const { instances, _view$ } = this;
-    const oldInstances = instances.splice(0, instances.length);
-
-    this.finalizeVegaInstances(oldInstances);
-    _view$.complete();
+    this.loader$.complete();
+    this.subscriptions.unsubscribe();
   }
 
-  async create(...args: Parameters<typeof embed>): Promise<void> {
-    const resultPromise = embed(...args);
-    this.instances.push(resultPromise);
+  create(...args: Parameters<typeof embed>): void {
+    const loader = embed(...args);
+    const subscriptions = this.subscriptions;
+    const teardown = () => loader.then(result => result.finalize());
+    const subscription = new Subscription(teardown);
+    const viewLoader = loader.then(result => result.view);
+    const observable = from(viewLoader).pipe(
+      map(view => ({ type: 'ready', view: view } as ViewReady)),
+      catchError(error => from([{ type: 'error', error } as ViewError])),
+      startWith({ type: 'loading', loader: viewLoader } as ViewLoading)
+    );
 
-    try {
-      const result = await resultPromise;
-      this.onResult(result, resultPromise);
-    } catch (error) {
-      this.onError(error, resultPromise);
-      throw error;
-    }
-  }
-
-  private onResult(result: Result, source: Promise<Result>): void {
-    const { instances, _view$ } = this;
-    const index = instances.indexOf(source);
-    if (index >= 0) {
-      const oldInstances = instances.splice(0, index);
-      this.finalizeVegaInstances(oldInstances);
-
-      _view$.next(result.view);
-    }
-  }
-
-  private onError(_error: unknown, source: Promise<Result>): void {
-    const { instances } = this;
-    const index = instances.indexOf(source);
-    if (index >= 0) {
-      instances.splice(index, 1);
-    }
-  }
-
-  private finalizeVegaInstances(instances: Promise<Result>[]): void {
-    instances.forEach(async instance => (await instance).finalize());
+    subscription.add(subscriptions);
+    loader.finally(() => subscriptions.unsubscribe());
+    this.subscriptions = subscription;
+    this.loader$.next(observable);
   }
 }
