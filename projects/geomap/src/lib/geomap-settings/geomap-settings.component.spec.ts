4286d89d1e3cda1ccee4fc11b44437d32cb1f609
axdanbol <daniel_bolin@yahoo.com>
2021-05-05 14:44:13 -0400

    Fixed geomap code smells

--- projects/geomap/src/lib/geomap-settings/geomap-settings.component.spec.ts
+++ projects/geomap/src/lib/geomap-settings/geomap-settings.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GeomapSettingsComponent } from './geomap-settings.component';
 
@@ -6,12 +6,12 @@ describe('GeomapSettingsComponent', () => {
   let component: GeomapSettingsComponent;
   let fixture: ComponentFixture<GeomapSettingsComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [ GeomapSettingsComponent ]
     })
     .compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(GeomapSettingsComponent);
--- projects/geomap/src/lib/geomap.component.spec.ts
+++ projects/geomap/src/lib/geomap.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GeomapComponent } from './geomap.component';
 
@@ -6,12 +6,12 @@ describe('GeomapComponent', () => {
   let component: GeomapComponent;
   let fixture: ComponentFixture<GeomapComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [ GeomapComponent ]
     })
     .compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(GeomapComponent);
